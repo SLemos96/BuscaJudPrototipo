10d45051c738ed1878b33f46421a934bc3de0cd0
Samuel Lemos <samuel.afonso2008@hotmail.com>
2021-12-16 17:08:19 -0300

    subindo informações sobre processos, bem como a comunicação desses dados entre o componente de tela e o componente card

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -9,6 +9,11 @@ export class CardComponent implements OnInit {
   
   constructor() { }
 
+  //variável sendo recebida do pai tela-busca.component
+  //cada mensagem recebida é um processo com sua documentação
+  //no próximo passo isso vai se tornar um model, e deixará de ser um array de string.
+  @Input() mensagem: string[] = [];
+  
   ngOnInit(): void {
   }
 
--- src/app/tela-busca/tela-busca.component.ts
+++ src/app/tela-busca/tela-busca.component.ts
@@ -10,6 +10,16 @@ export class TelaBuscaComponent implements OnInit {
 
   textoBusca = "testando"
 
+  mensagem = "blablabla"
+
+  //processos criados para preenchimento da informação visual. Apagar depois
+  //essa informação tá sendo enviada no tela-busca.component para o card.component. Linhas: 63 66 76 e 79.
+  //ideal é que no próximo passo seja feito com *ngFor
+  processo1: string[] = ['0800138-29.2015.4.05.8504', 'Juntada de Embargos de Declaração', 'DIREITO ADMINISTRATIVO E OUTRAS MATÉRIAS DE DIREITO PÚBLICO|Atos Administrativos|Improbidade Administrativa|Enriquecimento ilícito|'];
+  processo2: string[] = ['2000021-22.2019.4.05.8302', 'Conclusos para julgamento', 'DIREITO DO CONSUMIDOR|Contratos de Consumo|Seguro|'];
+  processo3: string[] = ['2000015-15.2019.4.05.8302', 'Conclusos para decisão', 'DIREITO ADMINISTRATIVO E OUTRAS MATÉRIAS DE DIREITO PÚBLICO|Atos Administrativos|Improbidade Administrativa|Enriquecimento ilícito|'];
+  processo4: string[] = ['2000006-53.2019.4.05.8302', 'Baixa Definitiva', 'DIREITO ADMINISTRATIVO E OUTRAS MATÉRIAS DE DIREITO PÚBLICO|Domínio Público|Imóvel Funcional|Reintegração de Posse|'];
+
   constructor(private telabuscaService: TelaBuscaService) { }
 
 
